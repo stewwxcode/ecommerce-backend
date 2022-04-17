2bfb1bd47a6a112df4f20306d91fe438ba9c227c
stewwxcode <stewartnadugaddi@gmail.com>
2022-04-17 16:39:23 +0530

    basic configuration to complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import express from "express";
+import express from 'express';
+import dotenv from 'dotenv'
+dotenv.config('./.env')
 
-console.log("hi")
+const app = express()
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT,()=>{
+   console.log(`server listening at port ${PORT}`)
+})
