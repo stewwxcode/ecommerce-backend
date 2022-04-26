6f32e4a8668429de9e46583009ce0785d678c4bd
stewwxcode <stewartnadugaddi@gmail.com>
2022-04-26 19:19:06 +0530

    Route Protection

---
+++ src/services/middlewares/isAuthenticated.js
@@ -0,0 +1,13 @@
+import { verifyJWT } from "../../utils";
+
+export const isAuthenticated = (req, res, next) => {
+
+    const token = req.headers["authorization"].split(" ")[1];
+    const data = verifyJWT(token);
+    if(data) return next()
+      return res.json({
+        data: {},
+        success: false,
+        message: "Unauthorised",
+      });
+};
