3dabc7964529ef5afafe03a6c07477c86407cc45
stewwxcode <stewartnadugaddi@gmail.com>
2022-04-27 19:20:36 +0530

    heroku change

--- src/services/mongodb/connectDB.js
+++ src/services/mongodb/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 export const connectDB = async ()=>{
     try{
           await mongoose.connect(process.env.DB_URI);
-          console.log('Connected to DB')
+          console.log('Connected to Database')
     }catch (error) {
         console.log(error)
     }
